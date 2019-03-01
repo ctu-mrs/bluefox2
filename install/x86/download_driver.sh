84db568c82fdfb2afa59998b631d6d2bd83d070e
Vojtech Spurny <vojtech.spurny@fel.cvut.cz>
2019-03-01 13:16:58 +0100

    removed SDK files -> download from MatrixVision server

---
+++ install/x86/download_driver.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# author: Tomas Baca
+
+curl  -O -J https://www.matrix-vision.com/USB2.0-single-board-camera-mvbluefox-mlc.html?file=tl_files/mv11/support/mvIMPACT_Acquire/01/mvBlueFOX-x86_64_ABI2-2.30.1.tgz 
