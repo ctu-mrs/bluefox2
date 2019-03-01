530b3fbefef8298a0145993143611d52fbf98f34
Vojtech Spurny <vojtech.spurny@fel.cvut.cz>
2019-03-01 13:31:44 +0100

    added install script

---
+++ install/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# author: Tomas Baca
+
+# install BLUEFOX
+UNAME_PROC=`uname -m`
+if [ "$UNAME_PROC" == "armv7l" ] ; then
+  echo "installing bluefox for armv71 architecture"
+  cd ~/git/uav_core/ros_packages/bluefox2/install/arm/
+  ./download_driver.sh
+  ./install_mvBlueFOX_ARM.sh --unattended
+else
+  echo "installing bluefox for x86_64 architecture"
+  cd ~/git/uav_core/ros_packages/bluefox2/install/x86_64/
+  ./download_driver.sh
+  ./install_mvBlueFOX.sh --unattended
+fi
