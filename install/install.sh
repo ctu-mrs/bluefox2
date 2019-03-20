89bbe5e5407e39e2c7454df79c9458217725e017
Viktor Walter <viktor_walter@outlook.com>
2019-03-20 19:24:08 +0100

    The install script for Bluefox still assumed that it is in uav_core

--- install/install.sh
+++ install/install.sh
@@ -5,12 +5,12 @@
 UNAME_PROC=`uname -m`
 if [ "$UNAME_PROC" == "armv7l" ] ; then
   echo "installing bluefox for armv71 architecture"
-  cd ~/git/uav_core/ros_packages/bluefox2/install/arm/
+  cd ~/git/uav_modules/ros_packages/bluefox2/install/arm/
   ./download_driver.sh
   ./install_mvBlueFOX_ARM.sh --unattended
 else
   echo "installing bluefox for x86_64 architecture"
-  cd ~/git/uav_core/ros_packages/bluefox2/install/x86_64/
+  cd ~/git/uav_modules/ros_packages/bluefox2/install/x86_64/
   ./download_driver.sh
   ./install_mvBlueFOX.sh --unattended
 fi
