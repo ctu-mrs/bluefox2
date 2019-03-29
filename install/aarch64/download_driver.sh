4beecf2a309b6c3e483dd608cbfa5b2c3adc9444
Jetson 1 <spurny.vojtech@fel.cvut.cz>
2019-03-29 11:45:53 +0100

    Adding binaries for aarch64, which is on the Jetson board

---
+++ install/aarch64/download_driver.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# author: Tomas Baca
+
+curl  -O -J https://www.matrix-vision.com/software-drivers-en.html?file=tl_files/mv11/support/mvIMPACT_Acquire/01/mvBlueFOX-ARM64_gnu-2.31.1.tgz
--- install/install.sh
+++ install/install.sh
@@ -5,9 +5,14 @@
 UNAME_PROC=`uname -m`
 if [ "$UNAME_PROC" == "armv7l" ] ; then
   echo "installing bluefox for armv71 architecture"
-  cd ~/git/uav_modules/ros_packages/bluefox2/install/arm/
+  cd ~/git/uav_modules/ros_packages/bluefox2/install/armv7l/
   ./download_driver.sh
   ./install_mvBlueFOX_ARM.sh --unattended
+elif [ "$UNAME_PROC" == "aarch64" ] ; then
+  echo "installing bluefox for aarch64 architecture"
+  cd ~/git/uav_modules/ros_packages/bluefox2/install/aarch64/
+  ./download_driver.sh
+  ./install_mvBlueFOX_AARCH64.sh --unattended
 else
   echo "installing bluefox for x86_64 architecture"
   cd ~/git/uav_modules/ros_packages/bluefox2/install/x86_64/
