ff9fddfd35aa3528f1c1720d822c2798e9e93cc3
Tomas Baca <tomas.baca@fel.cvut.cz>
2019-04-16 20:20:12 +0200

    udated bluefox2 driver

--- install/install.sh
+++ install/install.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-# author: Tomas Baca
+
+# get the path to this script
+MY_PATH=`dirname "$0"`
+MY_PATH=`( cd "$MY_PATH" && pwd )`
 
 # install BLUEFOX
 UNAME_PROC=`uname -m`
 if [ "$UNAME_PROC" == "armv7l" ] ; then
   echo "installing bluefox for armv71 architecture"
-  cd ~/git/uav_core/ros_packages/bluefox2/install/arm/
+  cd $MY_PATH/arm/
   ./download_driver.sh
   ./install_mvBlueFOX_ARM.sh --unattended
 else
   echo "installing bluefox for x86_64 architecture"
-  cd ~/git/uav_core/ros_packages/bluefox2/install/x86_64/
+  cd $MY_PATH/x86_64/
   ./download_driver.sh
   ./install_mvBlueFOX.sh --unattended
 fi
--- install/x86_64/download_driver.sh
+++ install/x86_64/download_driver.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# author: Tomas Baca
 
-curl  -O -J https://www.matrix-vision.com/USB2.0-single-board-camera-mvbluefox-mlc.html?file=tl_files/mv11/support/mvIMPACT_Acquire/01/mvBlueFOX-x86_64_ABI2-2.30.1.tgz 
+curl -O -J https://www.matrix-vision.com/USB2.0-industrial-camera-mvbluefox.html?file=tl_files/mv11/support/mvIMPACT_Acquire/01/mvBlueFOX-x86_64_ABI2-2.31.1.tgz 
