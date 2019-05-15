3d50bd9af424ae2d3c34f757c46870ec5a912a15
Vojtech Spurny <vojtech.spurny@fel.cvut.cz>
2019-05-15 10:38:50 +0200

    Update download_driver.sh

--- install/armv7l/download_driver.sh
+++ install/armv7l/download_driver.sh
@@ -4,5 +4,5 @@ curl  -O -J http://mrs.felk.cvut.cz/data/bluefox/mvBlueFOX-ARMhf_gnueabi-latest.
 rename_to=`cat mvBlueFOX-ARMhf_gnueabi-latest.tgz.version`
 curl -O -J http://mrs.felk.cvut.cz/data/bluefox/mvBlueFOX-ARMhf_gnueabi-latest.tgz 
 echo "renaming the latest to version name "$rename_to
-mv mvBlueFOX-x86_64_ABI2-latest.tgz $rename_to
-# curl  -O -J https://www.matrix-vision.com/USB2.0-single-board-camera-mvbluefox-mlc.html?file=tl_files/mv11/support/mvIMPACT_Acquire/01/mvBlueFOX-ARMhf_gnueabi-2.31.1.tgz
+mv mvBlueFOX-ARMhf_gnueabi-latest.tgz $rename_to
+rm *.version
