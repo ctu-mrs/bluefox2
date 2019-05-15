2679e0123d3af07fb7e23e7e3a3e080891f960c9
Vojtech Spurny <vojtech.spurny@fel.cvut.cz>
2019-05-15 10:37:22 +0200

    Update download_driver.sh

--- install/aarch64/download_driver.sh
+++ install/aarch64/download_driver.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # author: Tomas Baca
 
-curl  -O -J http://mrs.felk.cvut.cz/data/bluefox/mvBlueFOX-ARM64_gnu-latest.tgz
 curl -O -J http://mrs.felk.cvut.cz/data/bluefox/mvBlueFOX-ARM64_gnu-latest.tgz.version
 rename_to=`cat mvBlueFOX-ARM64_gnu-latest.tgz.version`
 curl -O -J http://mrs.felk.cvut.cz/data/bluefox/mvBlueFOX-ARM64_gnu-latest.tgz 
 echo "renaming the latest to version name "$rename_to
 mv mvBlueFOX-x86_64_ABI2-latest.tgz $rename_to
+rm *.version
