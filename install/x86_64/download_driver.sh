3838c87d42fda74dccb26ff57d9e46e2ff7f9709
Vojtech Spurny <vojtech.spurny@fel.cvut.cz>
2019-05-15 10:39:42 +0200

    Update download_driver.sh

--- install/x86_64/download_driver.sh
+++ install/x86_64/download_driver.sh
@@ -5,4 +5,4 @@ rename_to=`cat mvBlueFOX-x86_64_ABI2-latest.tgz.version`
 curl -O -J http://mrs.felk.cvut.cz/data/bluefox/mvBlueFOX-x86_64_ABI2-latest.tgz 
 echo "renaming the latest to version name "$rename_to
 mv mvBlueFOX-x86_64_ABI2-latest.tgz $rename_to
-# curl  -O -J https://www.matrix-vision.com/USB2.0-single-board-camera-mvbluefox-mlc.html?file=tl_files/mv11/support/mvIMPACT_Acquire/01/mvBlueFOX-x86_64_ABI2-2.31.1.tgz 
+rm *.version
