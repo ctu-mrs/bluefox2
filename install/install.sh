8a93f8f4fa345cd98e4aabe6d09f96acba262bae
Vojtech Spurny <vojtech.spurny@fel.cvut.cz>
2020-07-22 16:05:19 +0200

    updated install script

--- install/install.sh
+++ install/install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
 # get the path to this script
 MY_PATH=`dirname "$0"`
 MY_PATH=`( cd "$MY_PATH" && pwd )`
 
 DOWNLOAD_LINK="http://static.matrix-vision.com/mvIMPACT_Acquire"
 VERSION="2.34.0"
+
+echo "$0: Installing Bluefox SDK"
 
 # install BLUEFOX
 UNAME_PROC=`uname -m`
@@ -27,3 +34,5 @@ else
   ./download_driver.sh $DOWNLOAD_LINK $VERSION
   ./install_mvBlueFOX.sh --minimal
 fi
+
+echo "$0: done"
