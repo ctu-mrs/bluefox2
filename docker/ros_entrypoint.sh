f943a1cdaed0ab2559e4e5c95f675c095563404b
Jakub Lenner <lennejak@fel.cvut.cz>
2025-06-16 10:59:48 +0000

    fixed entrypoint

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -2,12 +2,21 @@
 
 . /opt/ros/jazzy/setup.sh
 
-if [-z UAV_NAME ]; then
+if [ -z "$UAV_NAME" ]; then
   echo "$0: the UAV_NAME environment variable is not set"
-  exit 1
+  #exit 1
 fi
 
-#cd /home/ubuntu/ws
-ros2 launch bluefox2 single.launch.py $@
+# Filter out the script name from arguments.
+# If you do not provide any arguments, the $@ will contain name of the script "/ros_entrypoint.sh",
+# which is invalid ROS argument and the node is not launched. If you provide some argument,
+# then it is ok, because the name of the script will not be present in $@, only provided arguments.
+LAUNCH_ARGS=""
+for arg in "$@"; do
+    if [[ "$arg" != *"ros_entrypoint.sh"* ]]; then
+        LAUNCH_ARGS="$LAUNCH_ARGS $arg"
+    fi
+done
 
-# docker run --privileged -it -v ./custom_config.yaml:/home/ubuntu/ws/custom_config.yaml mrs_realsense:latest custom_config:=custom_config.yaml
+#echo "Launch arguments: $LAUNCH_ARGS"
+ros2 launch bluefox2 single.launch.py $LAUNCH_ARGS
